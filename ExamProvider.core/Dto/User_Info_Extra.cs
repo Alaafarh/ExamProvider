afd918c3ea50976c4a2f6371da138aae8dfd08d8
Alaafarh <alaa.farhat@yahoo.com>
2024-06-25 18:02:59 +0300

    Add project files.

---
+++ ExamProvider.core/Dto/User_Info_Extra.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamProvider.core.Dto
+{
+    public class User_Info_Extra
+    {
+        public decimal UserId { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public DateTime? Datecreation { get; set; }
+        public DateTime? BirthDate { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public string? RoleName { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
---
+++ ExamProvider/Program.cs
@@ -0,0 +1,61 @@
+using ExamProvider.core.ICommon;
+using ExamProvider.core.IRepositary;
+using ExamProvider.core.IService;
+using ExamProvider.infra.Common;
+using ExamProvider.infra.Repositary;
+using ExamProvider.infra.Service;
+
+namespace ExamProvider
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            builder.Services.AddScoped<IDbContext, DbContext>();
+            builder.Services.AddScoped<IExamRepositary,ExamRepositary >();
+            builder.Services.AddScoped<IExamService,ExamService >();
+            builder.Services.AddScoped<IApiServiseReopsotary, ApiServiseReopsotary>();
+            builder.Services.AddScoped<IApiServiceServise, ApiServiceServise>();
+            builder.Services.AddScoped<ICompanyInfoReository, CompanyInfoReository>();
+            builder.Services.AddScoped<ICompanyInfoServise, CompanyInfoServise>();
+            builder.Services.AddScoped<IUserRoleRepository, UserRoleRepository>();
+            builder.Services.AddScoped<IUserRoleServise, UserRoleServise>();
+            builder.Services.AddScoped<IUserInfoRebository, UserInfoRebository>();
+            builder.Services.AddScoped<IUserInfoServise, UserInfoServise>();
+
+
+
+
+
+
+
+
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
